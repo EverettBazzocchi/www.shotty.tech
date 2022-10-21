8fc3777fee45b08592c80ea065beb93c773e1b79
Everett Bazzocchi <contact@everettbazzocchi.ca>
2022-10-21 12:31:15 -0500

    Staff Profile update

--- src/Pages/Staff/Components/Profile.js
+++ src/Pages/Staff/Components/Profile.js
@@ -1,6 +1,58 @@
+function timeSince(date) {
+    var seconds = Math.floor((new Date() - date) / 1000);
 
+    var interval = seconds / 31536000;
+
+    if (interval > 1) {
+        if (Math.floor(interval) === 1) {
+            return Math.floor(interval) + " year";
+        } else {
+            return Math.floor(interval) + " years";
+        }
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        if (Math.floor(interval) === 1) {
+            return Math.floor(interval) + " month";
+        } else {
+            return Math.floor(interval) + " months";
+        }
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+        if (Math.floor(interval) === 1) {
+            return Math.floor(interval) + " day";
+        } else {
+            return Math.floor(interval) + " days";
+        }
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+        if (Math.floor(interval) === 1) {
+            return Math.floor(interval) + " hour";
+        } else {
+            return Math.floor(interval) + " hours";
+        }
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+        if (Math.floor(interval) === 1) {
+            return Math.floor(interval) + " minute";
+        } else {
+            return Math.floor(interval) + " minutes";
+        }
+    }
+    if (Math.floor(seconds) === 1) {
+        return Math.floor(seconds) + " second";
+    } else {
+        return Math.floor(seconds) + " seconds";
+    }
+} 
 
 const Profile = ({ staff }) => {
+
+    let newDate = new Date(staff.timeAsStaff);
+    let time = timeSince(newDate);
 
     staff.image = "https://mc-heads.net/body/" + staff.username;
 
@@ -11,14 +63,19 @@ const Profile = ({ staff }) => {
                 <img src={staff.image} alt={staff.username} />
             </aside>
             <section>
-
                 <section className="Staff-Stats">
-                    <section className="Staff-Stat-Card fav-server">
+                    <section className="Staff-Stat-Card Stats-One">
                         <h4>Favorite Server</h4>
                         <p className="Stat-Counter">{staff.server}</p>
                     </section>
-                </section>
-                <section className="Staff-Stats">
+                    <section className="Staff-Stat-Card Stats-One">
+                        <h4>Rank</h4>
+                        <p className="Stat-Counter">{staff.rank}</p>
+                    </section>
+                    <section className="Staff-Stat-Card Stats-One">
+                        <h4>Time As Staff</h4>
+                        <p className="Stat-Counter">{time}</p>
+                    </section>
 
                     <section className="Staff-Stat-Card">
                         <h4>Bans</h4>
